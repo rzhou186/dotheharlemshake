b7c6b04074a8a335b720149da47eddddaebf0e63
Ray Zhou <rzhou186@stanford.edu>
2013-02-16 19:18:37 -0800

    Added scriptcam

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,20 +7,8 @@
 
 $(document).ready(function() {
 
-  $("#recorder").webcam({
-    
-    width: 398,
-    height: 298
-
-    onTick: function(remain) {
-
-        if (0 == remain) {
-            jQuery("#status").text("Cheese!");
-        } else {
-            jQuery("#status").text(remain + " seconds remaining...");
-        }
-    }
-
+  $("#recorder").scriptcam({
+    path: 'javascripts/scriptcam/'
   });
 
 });
