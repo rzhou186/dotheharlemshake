2ea85570174489f4e1c47e5af26bd1c703dfdd82
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-20 02:14:32 -0800

    Added absolute pathnames

--- videobuilder.php
+++ videobuilder.php
@@ -4,8 +4,8 @@
    	$file2 = $REQUEST['recordingTwo'];
    	$audio = $REQUEST['audioFile'];
    	$ffmpeg = 'ffmpeg';
-   	shell_exec($ffmpeg.' -i '.'home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file1.'.mpg');
-   	shell_exec($ffmpeg.' -i '.'home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file2.'.mpg');
+   	shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file1.'.mpg');
+   	shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file2.'.mpg');
    	/*unlink('home/scriptcam/'.$file1.'.mp4');
    	unlink('home/scriptcam/'.$file2.'.mp4');
    	exec('cat '.'home/scriptcam/'.$file1.'mpg '.'home/scriptcam/'.$file2.'mpg > '.'home/scriptcam/'.$file1.$file2.'mpg');
