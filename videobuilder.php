5a4ad0759ae13bb2eb9d5fed783b6eed2d560b04
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-20 02:04:34 -0800

    Fixed videobuilder

--- videobuilder.php
+++ videobuilder.php
@@ -2,6 +2,17 @@
    	
    	$file1 = $_REQUEST['recordingOne'];
    	$file2 = $REQUEST['recordingTwo'];
+   	$audio = $REQUEST['audioFile'];
    	$ffmpeg = 'ffmpeg';
-   	exec($ffmpeg.' -i '.$file1.' -i '.$file2.'output.mp4');
-?>
+   	exec($ffmpeg.' -i '.'home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file1.'.mpg');
+   	exec($ffmpeg.' -i '.'home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file2.'.mpg');
+   	unlink('home/scriptcam/'.$file1.'.mp4');
+   	unlink('home/scriptcam/'.$file2.'.mp4');
+   	exec('cat '.'home/scriptcam/'.$file1.'mpg '.'home/scriptcam/'.$file2.'mpg > '.'home/scriptcam/'.$file1.$file2.'mpg');
+   	unlink('home/scriptcam/'.$file1.'.mpg');
+   	unlink('home/scriptcam/'.$file2.'.mpg');
+   	exec($ffmpeg.' -i '.'home/scriptcam/'.$file1.$file2.'mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.$file1.$file2.'1.mpg');
+   	unlink('home/scriptcam/'.$file1.$file2.'.mpg');
+   	echo 'home/scriptcam/'.$file1.$file2.'1.mpg';
+  
+ ?>
