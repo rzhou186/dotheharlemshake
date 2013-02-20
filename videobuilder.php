d0528440772fb9fb3b88b9d32ff212774f98d615
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-20 02:12:51 -0800

    Changed exec to shell exec

--- videobuilder.php
+++ videobuilder.php
@@ -4,15 +4,15 @@
    	$file2 = $REQUEST['recordingTwo'];
    	$audio = $REQUEST['audioFile'];
    	$ffmpeg = 'ffmpeg';
-   	exec($ffmpeg.' -i '.'home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file1.'.mpg');
-   	exec($ffmpeg.' -i '.'home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file2.'.mpg');
-   	unlink('home/scriptcam/'.$file1.'.mp4');
+   	shell_exec($ffmpeg.' -i '.'home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file1.'.mpg');
+   	shell_exec($ffmpeg.' -i '.'home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'home/scriptcam/'.$file2.'.mpg');
+   	/*unlink('home/scriptcam/'.$file1.'.mp4');
    	unlink('home/scriptcam/'.$file2.'.mp4');
    	exec('cat '.'home/scriptcam/'.$file1.'mpg '.'home/scriptcam/'.$file2.'mpg > '.'home/scriptcam/'.$file1.$file2.'mpg');
    	unlink('home/scriptcam/'.$file1.'.mpg');
    	unlink('home/scriptcam/'.$file2.'.mpg');
    	exec($ffmpeg.' -i '.'home/scriptcam/'.$file1.$file2.'mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.$file1.$file2.'1.mpg');
    	unlink('home/scriptcam/'.$file1.$file2.'.mpg');
-   	echo 'home/scriptcam/'.$file1.$file2.'1.mpg';
+   	echo 'home/scriptcam/'.$file1.$file2.'1.mpg';*/
   
  ?>
