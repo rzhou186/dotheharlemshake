a1a824e06004c107a4a3ef45dd6ec29f28d081f5
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-19 23:28:18 -0800

    Echoing PHP

--- deletevid.php
+++ deletevid.php
@@ -1,10 +1,11 @@
 <?php
    	//	unlink($_POST['data']);
-   		$myfile = "testFile.txt";
+   	/*	$myfile = "testFile.txt";
    		$fh = fopen($myfile, 'w');
    		$stringData = "Floppy Jalopy\n";
 		fwrite($fh, $stringData);
 		$stringData = "Pointy Pinto\n";
 		fwrite($fh, $stringData);
-		fclose($fh);
+		fclose($fh);*/
+		echo "SUCCESSFUL";
 ?>
