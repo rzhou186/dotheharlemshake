f61245b0059199043b2848c7e20a602b957a2b8c
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-20 00:33:23 -0800

    Added videobuilder

---
+++ videobuilder.php
@@ -0,0 +1,9 @@
+<?php
+   	
+   	$file1 = $_REQUEST['recordingOne'];
+   	$file2 = $REQUEST['recordingTwo'];
+   	$ffmpeg = 'ffmpeg';
+   	exec($ffmpeg.' -i '.$file1.' -i '.$file2.'output.mp4');
+   	//unlink($data);
+   	echo "SUCCESSFUL";
+?>
