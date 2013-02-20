479d93f3274c4440c56b85568d9c1fd8e47ecb25
Ray Zhou <rzhou186@stanford.edu>
2013-02-20 04:17:06 -0800

    final changes to ffmpeg

--- videobuilder.php
+++ videobuilder.php
@@ -1,18 +1,18 @@
 <?php
       
-      $file1 = $_REQUEST['recordingOne'];
-      $file2 = $_REQUEST['recordingTwo'];
-      $audio = $_REQUEST['audioFile'];
-      $ffmpeg = 'ffmpeg';
-      shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file1.'.mpg');
-      unlink('/home/scriptcam/'.$file1.'.mp4');
-      shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file2.'.mpg');
-      unlink('/home/scriptcam/'.$file2.'.mp4');
-      shell_exec('cat '.'/home/scriptcam/'.$file1.'.mpg '.'/home/scriptcam/'.$file2.'.mpg > '.'/home/scriptcam/'.$file1.$file2.'.mpg');
-      unlink('/home/scriptcam/'.$file1.'.mpg');
-      unlink('/home/scriptcam/'.$file2.'.mpg');
-      shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.$file2.'.mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.'/home/scriptcam/'.$file1.$file2.'1.mpg');
-      unlink('/home/scriptcam/'.$file1.$file2.'.mpg');
-      echo '/home/scriptcam/'.$file1.$file2.'1.mpg';
+  $file1 = $_REQUEST['recordingOne'];
+  $file2 = $_REQUEST['recordingTwo'];
+  $audio = $_REQUEST['audioFile'];
+  $ffmpeg = 'ffmpeg';
+  shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file1.'.mpg');
+  unlink('/home/scriptcam/'.$file1.'.mp4');
+  shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file2.'.mpg');
+  unlink('/home/scriptcam/'.$file2.'.mp4');
+  shell_exec('cat '.'/home/scriptcam/'.$file1.'.mpg '.'/home/scriptcam/'.$file2.'.mpg > '.'/home/scriptcam/'.$file1.$file2.'.mpg');
+  unlink('/home/scriptcam/'.$file1.'.mpg');
+  unlink('/home/scriptcam/'.$file2.'.mpg');
+  shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.$file2.'.mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.'/home/scriptcam/'.$file1.$file2.'1.mpg');
+  unlink('/home/scriptcam/'.$file1.$file2.'.mpg');
+  // echo '/home/scriptcam/'.$file1.$file2.'1.mpg';
   
  ?>
