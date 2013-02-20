d57d531b237ea4763bb104236e33c9096af0f6f9
Ray Zhou <rzhou186@stanford.edu>
2013-02-20 02:30:01 -0800

    more debugging

--- videobuilder.php
+++ videobuilder.php
@@ -4,7 +4,7 @@
    	$file2 = $_REQUEST['recordingTwo'];
    	$audio = $_REQUEST['audioFile'];
    	$ffmpeg = 'ffmpeg';
-   	shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file1.'.mpg');
+   	// shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file1.'.mpg');
    	shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file2.'.mpg');
    	/*unlink('home/scriptcam/'.$file1.'.mp4');
    	unlink('home/scriptcam/'.$file2.'.mp4');
