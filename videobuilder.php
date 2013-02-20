c45c504fb33ad38d3dd498f45a30a42a88369f43
Ray Zhou <rzhou186@stanford.edu>
2013-02-20 03:40:04 -0800

    Stuff

--- videobuilder.php
+++ videobuilder.php
@@ -11,7 +11,7 @@
       shell_exec('cat '.'/home/scriptcam/'.$file1.'.mpg '.'/home/scriptcam/'.$file2.'.mpg > '.'/home/scriptcam/'.$file1.$file2.'.mpg');
       unlink('/home/scriptcam/'.$file1.'.mpg');
       unlink('/home/scriptcam/'.$file2.'.mpg');
-      //shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.$file2.'.mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.'/home/scriptcam/'.$file1.$file2.'1.mpg');
+      shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.$file2.'.mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.'/home/scriptcam/'.$file1.$file2.'1.mpg');
       //unlink('/home/scriptcam/'.$file1.$file2.'.mpg');
       echo '/home/scriptcam/'.$file1.$file2.'1.mpg';
   
