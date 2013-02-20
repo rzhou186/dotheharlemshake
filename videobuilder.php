aceefdc2f9f25d77b121cac3d98a606cafa989cd
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-20 02:52:38 -0800

    Running only till concat

--- videobuilder.php
+++ videobuilder.php
@@ -9,10 +9,10 @@
       shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file2.'.mpg');
       unlink('/home/scriptcam/'.$file2.'.mp4');
       shell_exec('cat '.'/home/scriptcam/'.$file1.'.mpg '.'/home/scriptcam/'.$file2.'.mpg > '.'/home/scriptcam/'.$file1.$file2.'.mpg');
-      unlink('/home/scriptcam/'.$file1.'.mpg');
-      unlink('/home/scriptcam/'.$file2.'.mpg');
-      shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.$file2.'mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.'/home/scriptcam/'.$file1.$file2.'1.mpg');
-      unlink('/home/scriptcam/'.$file1.$file2.'.mpg');
-      echo '/home/scriptcam/'.$file1.$file2.'1.mpg';
+      //unlink('/home/scriptcam/'.$file1.'.mpg');
+      //unlink('/home/scriptcam/'.$file2.'.mpg');
+      //shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.$file2.'mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.'/home/scriptcam/'.$file1.$file2.'1.mpg');
+      //unlink('/home/scriptcam/'.$file1.$file2.'.mpg');
+      //echo '/home/scriptcam/'.$file1.$file2.'1.mpg';
   
  ?>
