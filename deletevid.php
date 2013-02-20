1a19cc408320e89f22cfb8a25415068a7170d39f
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-19 23:37:01 -0800

    Fixed PHP

--- deletevid.php
+++ deletevid.php
@@ -1,11 +1,6 @@
 <?php
-   	//	unlink($_POST['data']);
-   	/*	$myfile = "testFile.txt";
-   		$fh = fopen($myfile, 'w');
-   		$stringData = "Floppy Jalopy\n";
-		fwrite($fh, $stringData);
-		$stringData = "Pointy Pinto\n";
-		fwrite($fh, $stringData);
-		fclose($fh);*/
-		echo "SUCCESSFUL";
+   	
+   	$data = $_POST['data'];
+   	unlink($data);
+   	echo "SUCCESSFUL";
 ?>
