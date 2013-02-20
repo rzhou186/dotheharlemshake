aa2820efc1b45ef7727f4d92a0d3a26ea58bd897
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-19 23:18:44 -0800

    Trying to make php work.

--- deletevid.php
+++ deletevid.php
@@ -1,5 +1,10 @@
 <?php
-   if(isset($_POST['data']) 
-   		unlink($_POST['data']);
-   		echo "SUCCESSFUL";
-  ?>
+   	//	unlink($_POST['data']);
+   		$myfile = "testFile.txt";
+   		$fh = fopen($myfile, 'w');
+   		$stringData = "Floppy Jalopy\n";
+		fwrite($fh, $stringData);
+		$stringData = "Pointy Pinto\n";
+		fwrite($fh, $stringData);
+		fclose($fh);
+?>
