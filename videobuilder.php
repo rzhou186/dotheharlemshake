43dc6e61ef4f432a485f9d21114abe3170d7ba77
Shubham Goel <gshubham@rescomp-12-242416.stanford.edu>
2013-02-20 02:46:58 -0800

    Finalized

--- videobuilder.php
+++ videobuilder.php
@@ -8,7 +8,7 @@
       unlink('/home/scriptcam/'.$file1.'.mp4');
       shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file2.'.mp4 -qscale:v 1 '.'/home/scriptcam/'.$file2.'.mpg');
       unlink('/home/scriptcam/'.$file2.'.mp4');
-      shell_exec('cat '.'/home/scriptcam/'.$file1.'mpg '.'/home/scriptcam/'.$file2.'mpg > '.'/home/scriptcam/'.$file1.$file2.'mpg');
+      shell_exec('cat '.'/home/scriptcam/'.$file1.'.mpg '.'/home/scriptcam/'.$file2.'.mpg > '.'/home/scriptcam/'.$file1.$file2.'.mpg');
       unlink('/home/scriptcam/'.$file1.'.mpg');
       unlink('/home/scriptcam/'.$file2.'.mpg');
       shell_exec($ffmpeg.' -i '.'/home/scriptcam/'.$file1.$file2.'mpg'.' -i '.$audio.' -ab 192k -vcodec copy -shortest '.'/home/scriptcam/'.$file1.$file2.'1.mpg');
