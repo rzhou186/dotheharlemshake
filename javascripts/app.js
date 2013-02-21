c767b7940f3d841abaced6339b8caafcce603172
Ray Zhou <rzhou186@stanford.edu>
2013-02-20 23:12:51 -0800

    tried fixing facebook like

--- javascripts/app.js
+++ javascripts/app.js
@@ -37,6 +37,9 @@ $(document).ready(function(){
 
   });
 
+  // Render social media sharing buttons visible
+  $(".media-sharer").css('visibility', 'visible');
+
   // Delete all associated files when client navigates away from webpage
   /*
   window.onbeforeunload = function() {
