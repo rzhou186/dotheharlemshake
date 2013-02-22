0c5c0eaf7cfe0f2de7b82dd15c799d8db10b46be
Ray Zhou <rzhou186@stanford.edu>
2013-02-21 22:02:34 -0800

    Final update to video JS

--- videobuilder.php
+++ videobuilder.php
@@ -18,7 +18,7 @@
     unlink('/home/scriptcam/'.$file1.$file2.'.mpg');
     shell_exec($ffmpeg.' -i '.'/var/www/dotheharlemshake/videos/'.$file1.$file2.'1.mpg -vcodec libx264 -qscale:v 1 -strict experimental '.'/var/www/dotheharlemshake/videos/'.$file1.$file2.'.mp4');
     unlink('/var/www/dotheharlemshake/videos/'.$file1.$file2.'1.mpg');
-    echo $file1.$file2.'.mpg';
+    echo $file1.$file2.'.mp4';
   }
 
 ?>
